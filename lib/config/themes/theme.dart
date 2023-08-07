069e7e1764af957ed0a5b9f66a7d82b0931a9f24
madedirga22 <madedirga22@gmail.com>
2023-08-07 12:18:11 +0800

    Project Init

---
+++ lib/config/themes/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff5C40CC);
+Color blackColor = const Color(0xff1F1449);
+Color whiteColor = const Color(0xffFFFFFF);
+Color greyColor = const Color(0xff9698A9);
+Color greenColor = const Color(0xff0EC3AE);
+Color redColor = const Color(0xffEB70A5);
+Color backgroundColor = const Color(0xffFAFAFA);
+Color inactiveColor = const Color(0xffDBD7EC);
+
+TextStyle blackTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: blackColor);
+
+TextStyle whiteTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: whiteColor);
+const greyTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: Color(0xff9698A9));
+const greenTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: Color(0xff0EC3AE));
+const redTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: Color(0xffEB70A5));
+const purpleTextStyle =
+    TextStyle(fontFamily: "Quicksand", color: Color(0xff5C40CC));
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
